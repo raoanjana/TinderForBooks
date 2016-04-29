ba57a6ed0e0b855003cb2d7dcce08145cc18fc34
Hayden Schmackpfeffer <hbs25@case.edu>
2016-04-29 19:55:24 -0400

    server stuff

---
+++ server/app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+# class EmotionAPI(Resource):
+#     def post(self):
+#         parser = reqparse.RequestParser()
+#         parser.add_argument('text', location='form', required=True)
+#         args = parser.parse_args()
+#
+#         emotion = comment_emotions.emotions(args['text'], g)
+#         sentic_values = emotion.get_all_sentic_values()
+#         compound_emotions = emotion.get_compound_emotion()
+#
+#         sentic_values = [e.name for e in sentic_values if e is not None]
+#         compound_emotions = [(e.name, v.name) for e, v in compound_emotions]
+#
+#         return {
+#             'sentic_values': sentic_values,
+#             'compound_emotions': compound_emotions
+#         }
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'You are being greeted by pageturner!'
+
+# api.add_resource(MultimediaAPI, '/search', endpoint='search')
+# api.add_resource(EmotionAPI, '/emotions', endpoint='emotions')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
